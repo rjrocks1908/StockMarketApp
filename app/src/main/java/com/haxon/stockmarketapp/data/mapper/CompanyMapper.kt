eb36f673efaffe9d1c91c19a49bc2e14e17a8fe9
Rahul Jain <rj.rahul1908@gmail.com>
2024-02-16 12:33:21 +0530

    Initial commit

---
+++ app/src/main/java/com/haxon/stockmarketapp/data/mapper/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.haxon.stockmarketapp.data.mapper
+
+import com.haxon.stockmarketapp.data.local.CompanyListingEntity
+import com.haxon.stockmarketapp.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
---
+++ app/src/main/java/com/haxon/stockmarketapp/data/remote/StockApi.kt
@@ -0,0 +1,18 @@
+package com.haxon.stockmarketapp.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListing(
+        @Query("apiKey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val API_KEY = "GBCYACUNLLZ5GA7C"
+        const val BASE_URL = "https://www.alphavantage.co"
+    }
+}
